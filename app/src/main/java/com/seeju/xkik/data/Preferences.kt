9a0d2a83d4a7a0df40a5a09bc8d6eccc9375982f
M Sajeel <msajeel417@gmail.com>
2020-09-10 16:25:44 +0500

    initial commit 2

--- app/src/main/java/com/seeju/xkik/data/Preferences.kt
+++ app/src/main/java/com/seeju/xkik/data/Preferences.kt
@@ -4,10 +4,7 @@ import com.jaqxues.akrolyb.prefs.Preference
 import com.jaqxues.akrolyb.prefs.Types.Companion.genericType
 
 
-/**
- * This file was created by Jacques Hoffmann (jaqxues) in the Project SnipTools.<br>
- * Date: 25.06.20 - Time 20:33.
- */
+
 object Preferences {
     //TODO for later
     val SELECTED_PACKS = null;
---
+++ packimpl/src/main/java/com/seeju/packimpl/PackImpl.java
@@ -0,0 +1,4 @@
+package com.seeju.packimpl;
+
+public class PackImpl {
+}
